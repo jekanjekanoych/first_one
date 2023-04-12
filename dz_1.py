2161409ad07474b25345788089cbe5db472c019d
jekanjekanoych <119631812+jekanjekanoych@users.noreply.github.com>
2023-04-12 21:14:36 +0300

    Update dz_1.py

--- dz_1.py
+++ dz_1.py
@@ -25,30 +25,3 @@ if __name__ == '__main__':
     assert parse('https://example.com/path/to/page?group=101&course=1') == {'group': '101', 'course': '1'}
     assert parse({'a': 1}) == 'must be a string'
     assert parse((1, 2, 3)) == 'must be a string'
-
-
-def parse_cookie(query: str) -> dict:
-    try:
-        a = query
-        cookie = SimpleCookie()
-        cookie.load(a)
-        cookies = {k: v.value for k, v in cookie.items()}
-        print(cookies)
-        return cookies
-
-    except AttributeError:
-        return "must be a string"
-
-
-if __name__ == '__main__':
-    assert parse_cookie('name=Dima;') == {'name': 'Dima'}
-    assert parse_cookie('') == {}
-    assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
-    assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
-    assert parse_cookie('/') == {}
-    assert parse_cookie('devicePixelRatio=1;') == {'devicePixelRatio': '1'}
-    assert parse_cookie('__utmc=13103656942;') == {'__utmc': '13103656942'}
-    assert parse_cookie('__utmz=13105942.1.1.1.utmcsr=google|utmccn=(organic);') == \
-           {'__utmz': '13105942.1.1.1.utmcsr=google|utmccn=(organic)'}
-    assert parse({'b': -2}) == 'must be a string'
-    assert parse((3, 2, 1)) == 'must be a string'
